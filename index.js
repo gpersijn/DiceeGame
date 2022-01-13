35318083ceedd2c7caddf04bc27c8f7a68d3b1bf
Gustave Persijn <gpersijn@outlook.com>
2022-01-12 23:33:03 -0300

    #2 - Adicionando funcionalidade ao codigo

---
+++ index.js
@@ -0,0 +1,18 @@
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+let randomDiceImage = "images/dice" + randomNumber1 + ".png";
+let randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll(".img1")[0].setAttribute("src", randomDiceImage);
+document.querySelectorAll(".img1")[1].setAttribute("src", randomDiceImage2);
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector(".topic").textContent = "😜 Play 1 Wins!";
+} else if(randomNumber1 < randomNumber2){
+    document.querySelector(".topic").textContent = "🤩 Play 2 Wins!";
+} else {
+    document.querySelector(".topic").textContent = "😬 Draw!";
+}
+   
+
